e4dc66054d5a785ba3fca86107b81ca14c3fd14c
NikitaJTelisara <nikitatelisara@gmail.com>
2017-05-13 18:22:54 -0500

    Update stringManipulation.java

--- stringManipulation.java
+++ stringManipulation.java
@@ -96,7 +96,7 @@ public class stringManipulation {
             charset[val] = true;
         }
         return true;
-    }
+    }/* can either use hashset too */
 
     /* reverse a string */
     public static String stringReversal(String string) {
